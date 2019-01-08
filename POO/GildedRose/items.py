6b62310de855ac421cc62718234b02fb8f4d09d1
unknown <claudiamariamail97@gmail.com>
2019-01-08 18:27:33 +0100

    updateable tested

--- POO/GildedRose/items.py
+++ POO/GildedRose/items.py
@@ -2,22 +2,27 @@ from updateable import Updateable
 
 class Item(Updateable):
 
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
+
     def getName(self):
         return self.name
+
 
     def getSellIn(self):
         return self.sell_in
 
+
     def getQuality(self):
         return self.quality
 
+
     def __repr__(self):
-        return '%s, %s, %s' % (self.name, self.sell_in, self.quality)
+        return '%s, %s, %s' % (self.getName(), self.getSellIn(), self.getquality())
 
 
 
