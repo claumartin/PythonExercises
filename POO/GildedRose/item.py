b89aa311a0a468aa98c59c3123a25dfccf8c00ad
unknown <claudiamariamail97@gmail.com>
2019-01-09 18:51:48 +0100

    no getters in class Item refactor

--- POO/GildedRose/item.py
+++ POO/GildedRose/item.py
@@ -7,18 +7,6 @@ class Item:
         self.quality = quality
 
 
-    def getName(self):
-        return self.name
-
-
-    def getSellIn(self):
-        return self.sellIn
-
-
-    def getQuality(self):
-        return self.quality
-
-
     def __repr__(self):
         return '%s, %s, %s' % (self.getName(), self.getSellIn(), self.getquality())
 
