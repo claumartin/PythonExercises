74e86faf1773588037db80bf39243ed6019440ac
unknown <claudiamariamail97@gmail.com>
2019-01-09 18:03:43 +0100

    regularItem tested

---
+++ POO/GildedRose/regularItem.py
@@ -0,0 +1,48 @@
+from item import Item
+from updateable import Updateable
+
+
+class RegularItem(Item, Updateable):
+    def __init__(self, name, sellIn, quality):
+        Item.__init__(self, name, sellIn, quality)
+
+    def setSellIn(self):
+        self.sellIn -= 1
+
+
+    def updateQuality(self):
+
+        if self.getSellIn() >= 0:
+            self.quality -= 1
+        
+        else:
+            self.quality -= 2
+
+
+    def setQuality(self):
+        
+        if updateQuality() >= 50:
+            self.quality = 50
+
+        elif updateQuality() < 0:
+            self.quality = 0
+
+        else:
+            self.quality = updateQuality(self)
+
+        assert 0 <= self.quality <= 50, '%s quality de out of range' % self.__class__.__name__
+
+
+
+
+if __name__ == '__main__':
+
+    pato = RegularItem('pato', 20, 4)
+
+    assert pato.getName() == 'pato'
+
+    pato.setSellIn()
+    assert pato.getSellIn() == 19
+
+    pato.updateQuality()
+    assert pato.getQuality() == 3
