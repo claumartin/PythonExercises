bdfeb6b223916ee6d0a2ba79a175e5daa70ef57d
unknown <claudiamariamail97@gmail.com>
2019-01-09 18:27:07 +0100

    sulfuras.py tested

---
+++ POO/GildedRose/sulfuras.py
@@ -0,0 +1,22 @@
+from regularItem import RegularItem
+
+class Sulfuras(RegularItem):
+
+    def setSellIn(self):
+        pass
+
+    def updateQuality(self):
+        pass
+
+
+if __name__ == '__main__':
+
+    pato = Sulfuras('pato', 0, 80)
+
+    assert pato.getName() == 'pato'
+
+    pato.setSellIn()
+    assert pato.getSellIn() == 0
+
+    pato.updateQuality() 
+    assert pato.getQuality() == 80
