dbe4d76df8bbf11d0850f68aa4f88d89658c4c48
unknown <claudiamariamail97@gmail.com>
2019-01-15 12:23:08 +0100

    getHora() tested

--- POO/Hora/hora.py
+++ POO/Hora/hora.py
@@ -5,9 +5,22 @@ class Hora:
         self.hora = hora
         self.minuto = minuto
         self.segundo = segundo
+    
+
+
+    def getHora(self):
+
+        listHora = []
+        for key in self.__dict__:
+            listHora.append(self.__dict__[key])
+
+        return listHora
         
 
 
+    def __repr__(self):
+
+        return self.getHora()
 
 
 
@@ -21,4 +34,6 @@ class Hora:
 if __name__ == "__main__":
     
     miHora = Hora(11, 30, 00)
-    assert print(miHora) # It is '11:30:0'
+    print(miHora) # It is '11:30:0'
+    
+    assert miHora.getHora() == [11, 30, 00]
